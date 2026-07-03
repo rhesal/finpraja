bffac5b030c876bbedc57566f585b4f9147219a2
rhesal <rhesal.mahadyanto@gmail.com>
2026-07-03 21:29:17 +0700

    fix: buat konfigurasi base path dinamis untuk Vercel dan GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/finpraja/',
+  // base: '/finpraja/',
+  base: process.env.VERCEL ? '/' : '/finpraja/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
